c09992f98caa319497c10989d423353c8b683cd0
Sahil Kumar <72195146+SahilKumarIN@users.noreply.github.com>
2023-11-10 16:13:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config() ;
-const port = process.env.PORT || 8080 ;
 const server = express();
-mongoose.connect(process.env.URI+`myUsers`);
-// console.log('db connected');
+require('dotenv').config() ;
+const port = process.env.PORT || 8080 ;
+mongoose.connect(process.env.URI+`/myUsers`);
+console.log('db connected');
 
 const userSchema = new mongoose.Schema({
     username: String,
@@ -46,18 +46,18 @@ server.get('/get-user' , (req,res)=>{
             res.send(foundUser);
         })
         .catch (err=> {
-            alert("Currently No User Found!!!")
+            console.log(err)
         });
 });
 
 server.post('/del-user' , (req,res)=>{
     const id = req.body.id;
-    User.findByIdAndDelete(id).then(res.send("User Deleted")).catch(err=>{alert("There was a problem deleting the user.");})
+    User.findByIdAndDelete(id).then(res.send("User Deleted")).catch(err=>{console.log(err);})
 });
 
 
 
 server.listen(port ,()=>{
-    // console.log("Server Started at "+port+"....");
+    console.log("Server Started at "+port+"....");
     // console.log("Server started");
 })
